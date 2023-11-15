226a1f0ea7d5e0cab766fbdf07f8bebbc188a47e
Jaeho Yi <akghxhs55@gmail.com>
2023-11-15 23:55:45 +0900

    initial commit

---
+++ TodoList/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+var userChoice = Console.ReadLine()!;
+bool isLong = IsLong(userChoice);
+
+if (userChoice == "S") {
+    PrintSelectedOption("See all TODOs");
+}
+else if (userChoice == "A") {
+    PrintSelectedOption("Add a TODO");
+}
+else if (userChoice == "R") {
+    PrintSelectedOption("Remove a TODO");
+}
+else if (userChoice == "E") {
+    PrintSelectedOption("Exit");
+}
+
+
+void PrintSelectedOption(string selectedOption) {
+    Console.WriteLine("Selected option: " + selectedOption);
+}
+
+bool IsLong(string input) {
+    return input.Length > 10;
+}
